767e4630933b4d3351830a8d82123693bf2c3d42
bruce <cnhxunoo@gmail.com>
2024-02-21 18:17:45 +0800

    add clickedlabel

--- llama_python_gui/views/components/__init__.py
+++ llama_python_gui/views/components/__init__.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
 
 
@@ -12,6 +13,18 @@ class ArchiveChats(QWidget):
         self.setLayout(QVBoxLayout())
 
         for _ in range(30):
-            q_label = QLabel("chat")
+            q_label = ClickLable("chat")
             q_label.setFixedHeight(30)
             self.layout().addWidget(q_label)
+
+
+class ClickLable(QLabel):
+
+    clicked = Signal()
+
+    def __init__(self, text: str) -> None:
+        super().__init__(text)
+
+    def mousePressEvent(self, event):
+        self.clicked.emit()
+        print("clicked")
