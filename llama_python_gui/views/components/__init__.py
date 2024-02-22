d2975d1428639bdca93b15b3c74e2c929fcace8c
bruce <cnhxunoo@gmail.com>
2024-02-22 21:40:51 +0800

    style single chat widget

--- llama_python_gui/views/components/__init__.py
+++ llama_python_gui/views/components/__init__.py
@@ -1,5 +1,8 @@
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget, QHBoxLayout
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget, QHBoxLayout
+
+from llama_python_gui.assets import resource  # noqa
 
 
 class ArchiveChats(QWidget):
@@ -7,15 +10,21 @@ class ArchiveChats(QWidget):
     def __init__(self):
         super().__init__()
         self.addComponents()
+        self.addStyle()
 
     def addComponents(self):
 
         self.setLayout(QVBoxLayout())
+        self.layout().setContentsMargins(5, 2, 5, 2)
+        self.layout().setSpacing(1)
 
         for _ in range(30):
             q_label = SingleChat("chat")
-            q_label.setFixedHeight(30)
+            q_label.setObjectName("chat")
             self.layout().addWidget(q_label)
+
+    def addStyle(self):
+        self.setStyleSheet("#chat{border: 1px solid #fff; padding:2px;}")
 
 
 class ClickLable(QLabel):
@@ -39,19 +48,29 @@ class SingleChat(QWidget):
     def __init__(self, content: str):
         super().__init__()
         self.content = content
-        self.setFixedHeight(50)
+        self.setFixedSize(170, 37)
         self.addComponents()
         self.addStyle()
         self.addConnections()
 
     def addComponents(self):
         self.setLayout(QHBoxLayout())
-        self.layout().addWidget(QLabel(self.content))
-
         self.layout().setContentsMargins(0, 0, 0, 0)
 
+        self.content = QLabel(self.content)
+        self.content.setFixedWidth(100)
+        self.layout().addWidget(self.content)
+
+        self.edit_btn = QPushButton()
+        self.edit_btn.setIcon(QPixmap(":/icons/edit.svg"))
+        self.layout().addWidget(self.edit_btn)
+
+        self.delete_btn = QPushButton()
+        self.delete_btn.setIcon(QPixmap(":/icons/trash-2.svg"))
+        self.layout().addWidget(self.delete_btn)
+
     def addStyle(self):
-        self.setStyleSheet("*{border-bottom: 1px solid #424242;}")
+        self.setStyleSheet("QLabel, QPushButton{border: none;}")
 
     def addConnections(self):
         pass
