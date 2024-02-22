8d8d5e43142231b80a1a04ea07c23f940f592e8b
bruce <cnhxunoo@gmail.com>
2024-02-22 17:05:49 +0800

    add singlechat item

--- llama_python_gui/views/components/__init__.py
+++ llama_python_gui/views/components/__init__.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget, QHBoxLayout
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget, QHBoxLayout
 
 
 class ArchiveChats(QWidget):
@@ -13,7 +13,7 @@ class ArchiveChats(QWidget):
         self.setLayout(QVBoxLayout())
 
         for _ in range(30):
-            q_label = ClickLable("chat")
+            q_label = SingleChat("chat")
             q_label.setFixedHeight(30)
             self.layout().addWidget(q_label)
 
@@ -26,6 +26,35 @@ class ClickLable(QLabel):
         super().__init__(text)
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(QLabel("hello"))
+
+    def mousePressEvent(self, event):
+        self.clicked.emit()
+        print("clicked")
+
+
+class SingleChat(QWidget):
+
+    clicked = Signal()
+
+    def __init__(self, content: str):
+        super().__init__()
+        self.content = content
+        self.setFixedHeight(50)
+        self.addComponents()
+        self.addStyle()
+        self.addConnections()
+
+    def addComponents(self):
+        self.setLayout(QHBoxLayout())
+        self.layout().addWidget(QLabel(self.content))
+
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
+    def addStyle(self):
+        self.setStyleSheet("*{border-bottom: 1px solid #424242;}")
+
+    def addConnections(self):
+        pass
 
     def mousePressEvent(self, event):
         self.clicked.emit()
